23f28ec8d7994fe25155909e8f6746e7a670e7b8
Mariya Pasichnyk <mariyapasechnik1992@gmail.com>
2026-02-09 11:57:03 +0200

    Update GaragePage2.cy.js

--- cypress/support/pageObjects/GaragePage2.cy.js
+++ cypress/support/pageObjects/GaragePage2.cy.js
@@ -32,12 +32,11 @@ class AddFuel {
         this.openAddModal();
         
         this.modal.within(() => {
-            
             this.mileage.should('not.have.value', '');
-            
+
             this.mileage.invoke('val').then((currentValue) => {
                 const newMileage = Number(currentValue) + 10;
-                
+
                 this.mileage.clear().type(newMileage);
                 this.liters.clear().type(liters);
                 this.cost.clear().type(cost);
